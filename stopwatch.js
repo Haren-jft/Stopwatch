be0edc21d635e09ea5da587eab063c9d044ec993
Haren-jft <haren.sharma@jellyfishtechnologies.com>
2022-11-17 11:00:31 +0530

    stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,57 @@
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startBtn.addEventListener('click', function () {
+	timer = true;
+	stopWatch();
+});
+
+stopBtn.addEventListener('click', function () {
+	timer = false;
+});
+
+resetBtn.addEventListener('click', function () {
+	timer = false;
+	hour = 0;
+	minute = 0;
+	second = 0;
+	count = 0;
+	document.getElementById('hr').innerHTML = "00";
+	document.getElementById('min').innerHTML = "00";
+	document.getElementById('sec').innerHTML = "00";
+	document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch() {
+	if (timer) {
+		count++;
+
+		if (count == 100) {
+			second++;
+			count = 0;
+		}
+
+		if (second == 60) {
+			minute++;
+			second = 0;
+		}
+
+		if (minute == 60) {
+			hour++;
+			minute = 0;
+			second = 0;
+		}
+
+		document.getElementById('hr').innerHTML = hour;
+		document.getElementById('min').innerHTML = minute;
+		document.getElementById('sec').innerHTML = second;
+		document.getElementById('count').innerHTML = count;
+		setTimeout(stopWatch, 10);
+	}
+}
